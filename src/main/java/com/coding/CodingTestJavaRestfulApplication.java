48e5b4a274b98c7ddbab24902adfff8ea27996ef
vijaypdwivedi <vijay.dwivedi@outlook.com>
2024-02-26 21:24:19 +0530

    added project base folder

---
+++ src/main/java/com/coding/CodingTestJavaRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestJavaRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTestJavaRestfulApplication.class, args);
+	}
+
+}

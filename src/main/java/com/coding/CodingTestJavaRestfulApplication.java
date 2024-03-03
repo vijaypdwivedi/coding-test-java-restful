5e8c34c524c526ba0d770a9c0753eb98d2ff844e
vijaypdwivedi <vijay.dwivedi@outlook.com>
2024-03-03 11:24:36 +0530

    added base classes

--- src/main/java/com/coding/CodingTestJavaRestfulApplication.java
+++ src/main/java/com/coding/CodingTestJavaRestfulApplication.java
@@ -1,10 +1,15 @@
 package com.coding;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class CodingTestJavaRestfulApplication {
+public class CodingTestJavaRestfulApplication  {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodingTestJavaRestfulApplication.class, args);
---
+++ src/main/java/com/coding/repository/BookRepository.java
@@ -0,0 +1,4 @@
+package com.coding.repository;
+
+public interface BookRepository {
+}
